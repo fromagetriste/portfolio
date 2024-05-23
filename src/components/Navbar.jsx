3bd508b8b94a5aef7f381dddc2c48f141112ad9c
unknown <fromagetriste@gmail.com>
2024-05-23 07:53:03 -0600

    environment all set

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav__container">
+        <ul className="nav-left__container">
+          <li>
+            <img
+              src="/backgrounds-and-icons/react-favicon.svg"
+              alt="logo-react"
+              className="nav-logo"
+            ></img>
+          </li>
+          <li>React JS</li>
+        </ul>
+
+        <ul className="nav-right__container">
+          <li>
+            <a href="#">Stack</a>
+          </li>
+          <li>
+            <a href="#">Skills</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/fromagetriste/"
+              target="_blank"
+            >
+              <img
+                src="/linkedin.svg"
+                alt="logo-Linkedin"
+                className="nav-logo"
+                id="saturate-img"
+              ></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/fromagetriste" target="_blank">
+              <img
+                src="/github-mark-white.svg"
+                alt="logo-github"
+                className="nav-logo"
+              ></img>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
