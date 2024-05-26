bae2d313c06b1e16bc082890edd8ef7fd6262e10
unknown <fromagetriste@gmail.com>
2024-05-26 13:38:53 -0600

    added colors to terminal

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 const HeroSection = () => {
   return (
     <main className="hero__container">
@@ -27,6 +29,7 @@ const HeroSection = () => {
       </div>
 
       {/* Right-container (terminal) starts here */}
+
       <div className="terminal__container">
         <div className="upper-part__container">
           <img
@@ -46,10 +49,74 @@ const HeroSection = () => {
           ></img>
         </div>
         <div className="code-text__container">
+          {/* to have a text being not interpreted by JSX, set this around : {"your text"} */}
           <p>
-            Hello, <br />
-            My name is Damien, I am <br />a self-taught front-end <br />
-            Developer.
+            <span className="my-purple">import</span>{" "}
+            <span className="my-red">React</span>,{" "}
+            <span className="my-orange">{"{"}</span>{" "}
+            <span className="my-red">{" useState "}</span>{" "}
+            <span className="my-orange">{"}"}</span>{" "}
+            <span className="my-purple">from</span>{" "}
+            <span className="my-green">
+              {"'"}react{"'"}
+            </span>
+            ; <br />
+            <br />
+            <span className="my-purple">const</span>{" "}
+            <span className="my-blue">Developer = </span>
+            <span className="my-orange">()</span>{" "}
+            <span className="my-purple">={"> "}</span>
+            <span className="my-orange">{"{"}</span>
+            <br />
+            <span className="my-purple">[</span>
+            <span className="my-red">portfolio</span>,
+            <span className="my-red">setPortfolio</span>
+            <span className="my-purple">]</span>{" "}
+            <span className="my-blue">= useState</span>
+            <span className="my-purple">(</span>
+            <span className="my-orange">true</span>
+            <span className="my-purple">)</span>
+            <br />
+            <br />
+            <span className="my-purple">return {"("}</span>
+            <br />
+            {"<"}
+            <span className="my-red">main</span>
+            {">"} <br />
+            {"<"}
+            <span className="my-red">h1</span>
+            {">"} <br />
+            Hello, My name is Damien, I am a self-taught front-end Developer.{" "}
+            <br />
+            {"<"}
+            <span className="my-red">h1</span>
+            {">"} <br />
+            {"<"}
+            <span className="my-red">p</span>
+            {">"} <br />I am driven by beautiful, robust and user friendly
+            interfaces. <br />
+            {"<"}
+            <span className="my-red">p</span>
+            {"/>"} <br />
+            {"<"}
+            <span className="my-red">div</span>
+            {">"} <br />
+            {"<"}
+            <span className="my-red">button</span>
+            {">"} Get my Resume {"<"}
+            <span className="my-red">button</span>
+            {"/>"} <br />
+            {"<"}
+            <span className="my-red">button</span>
+            {">"} Contact me {"<"}
+            <span className="my-red">button</span>
+            {">"} <br />
+            {"<"}
+            <span className="my-red">div</span>
+            {">"} <br />
+            {"<"}
+            <span className="my-red">main</span>
+            {"/>"} <br /> <br />
           </p>
         </div>
       </div>
