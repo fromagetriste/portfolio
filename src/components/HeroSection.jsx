7b5f5008bf673dc16330c53eb956899e287bd86b
unknown <fromagetriste@gmail.com>
2024-05-26 05:49:19 -0600

    started terminal component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
   return (
-    <main className="flex__container">
+    <main className="hero__container">
       {/* Left-container starts here */}
       <div className="card__container">
         <h1>
@@ -29,18 +29,29 @@ const HeroSection = () => {
       {/* Right-container (terminal) starts here */}
       <div className="terminal__container">
         <div className="upper-part__container">
-          <li> a </li>
-          <li> b </li>
-          <li> c </li>
+          <img
+            src="/backgrounds-and-icons/bullet-point-terminal.svg"
+            alt="bullet point for style"
+            id="bullet-point"
+          ></img>
+          <img
+            src="/backgrounds-and-icons/bullet-point-terminal.svg"
+            alt="bullet point for style"
+            id="bullet-point"
+          ></img>
+          <img
+            src="/backgrounds-and-icons/bullet-point-terminal.svg"
+            alt="bullet point for style"
+            id="bullet-point"
+          ></img>
         </div>
-        <p>
-          const Damien =
-          <br />
-          name : Damien GRAVELLE <br />
-          Date of birth : 14/03/1992 at Valenciennes <br />
-          Main Stack : [React, TypeScript, Redux,MySql] <br />
-          Secondary Stack : [Python, Django, Panda]
-        </p>
+        <div className="code-text__container">
+          <p>
+            Hello, <br />
+            My name is Damien, I am <br />a self-taught front-end <br />
+            Developer.
+          </p>
+        </div>
       </div>
     </main>
   );
