78523e76c6ff45c1055d506f88c1c3d8d6315643
fromagetriste __ <fromagetriste@gmail.com>
2025-01-28 13:35:21 -0600

    finished the about section

---
+++ src/components/myText.js
@@ -0,0 +1,35 @@
+export const myText = {
+    english: {
+        title1: "How it's like working with me",
+        answer1: `I am reliable and always happy to help. I am not afraid of delivering clean code under tight deadlines and I've worked under pressure in the past. Although I can work independently, I prefer being part of a team. At work, I'm dedicated and hardworking, but I also enjoy bringing humor to the team.`,
+        title2: "Why Web Development ?",
+        answer2: `I've always tended to improve our process, with communication and also tools like Excel. Then I felt limited so I learned Python, and I once needed to deploy an executable program so I hosted with Django, and fell in love with web development. So I learned JS and React. I would say I'm very curious about any tech in general, from hardware to user's experience.`,
+    },
+    francais: {
+        title1: "Travailler avec moi au quotidien",
+        answer1: `Je suis fiable et toujours prêt à aider. Je n’ai pas peur de livrer du code propre dans des délais serrés, et j’ai déjà travaillé sous pression par le passé. Bien que je puisse travailler de manière autonome, je préfère faire partie d’une équipe. Au travail, je suis dévoué et travailleur, mais j’aime aussi apporter de l’humour à l’équipe.`,
+        title2: "Pourquoi le développement web ?",
+        answer2: `J'ai toujours oeuvrer à améliorer les processus, que ce soit par la communication ou avec des outils comme Excel. Avec le temps, je me suis senti limité par Excel, alors j'ai appris Python. Un jour, j'ai dû déployer un programme exécutable, alors je l'ai hébergé avec Django, et je suis tombé amoureux du développement web. J'ai donc appris JS et React. Je dirais que je suis très curieux de la technologie en général, qu'il s'agisse de matériel ou de l'expérience utilisateur.`,
+    },
+    espanol: {
+        title1: "Cómo es trabajar conmigo",
+        answer1: `Soy confiable y siempre estoy dispuesto a ayudar. No tengo miedo de entregar código limpio dentro de plazos ajustados, y he trabajado bajo presión en el pasado. Aunque puedo trabajar de manera independiente, prefiero formar parte de un equipo. En el trabajo, soy dedicado y trabajador, pero también me gusta aportar humor al equipo.`,
+        title2: "¿Por qué el desarrollo web?",
+        answer2: `Siempre he tendido a mejorar nuestros procesos, ya sea con la comunicación o con herramientas como Excel. Luego me sentí limitado, así que aprendí Python. Una vez necesité desplegar un programa ejecutable, así que lo alojé con Django, y me enamoré del desarrollo web. Por eso aprendí JS y React. Diría que soy muy curioso con cualquier tecnología en general, desde hardware hasta la experiencia del usuario.`,
+    },
+};
+
+export const flagz = [
+    {
+        tongue: "english",
+        src: "/public/icons/about/uk.svg",
+    },
+    {
+        tongue: "francais",
+        src: "/public/icons/about/fr.svg",
+    },
+    {
+        tongue: "espanol",
+        src: "/public/icons/about/es.svg",
+    },
+];
